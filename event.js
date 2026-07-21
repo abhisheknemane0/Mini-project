27a050c52ed4e11197f8ae5fa62fa19e7f2ac7f4
Abhishek Nemane <abhisheknemane0@gmail.com>
2026-07-21 16:57:22 +0530

    Create event.js

---
+++ event.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("click",()=> {
+    document.querySelector(".box").innerHTML = "<b> Yayy you were clicked</b> enjoy your click!"
+})
